9cfe12de74ff4bf907c2d5bfc3b393cba2ac359a
jeff.ward <jeff.ward@nimbee.com>
2013-08-14 13:02:47 -0600

    Fixed stream_flv.sh

--- stream_flv.sh
+++ stream_flv.sh
@@ -1,1 +1,1 @@
-vlc ~/Downloads/BigBuckBunny_DivX_HD720p_ASP.divx --sout '#transcode{vcodec=h264,vb=2048,acodec=mp3,ab=128,samplerate=44100}:std{access=http{mime=video/x-flv},mux=ffmpeg{mux=flv},dst=0.0.0.0:8081/stream.flv}'
+vlc ~/Downloads/BigBuckBunny_DivX_HD720p_ASP.divx --sout '#transcode{vcodec=FLV1,vb=4096,acodec=mpga,ab=128,samplerate=44100}:std{access=http{mime=video/x-flv},mux=ffmpeg{mux=flv},dst=0.0.0.0:8081/stream.flv}'
